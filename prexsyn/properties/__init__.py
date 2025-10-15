0a14da9bf183bb62659bc948c62966be800d62c4
Shitong Luo <luost26@gmail.com>
2025-10-15 16:29:28 -0400

    explicitly export classes

--- prexsyn/properties/__init__.py
+++ prexsyn/properties/__init__.py
@@ -1,1 +1,4 @@
-from .base import BasePropertyDef, BasePropertyEmbedder, PropertyEmbedding, PropertySet
+from .base import BasePropertyDef as BasePropertyDef
+from .base import BasePropertyEmbedder as BasePropertyEmbedder
+from .base import PropertyEmbedding as PropertyEmbedding
+from .base import PropertySet as PropertySet
