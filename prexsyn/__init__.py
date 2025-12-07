45e6e2671a93886b2fc9d394719efac805f70d16
Shitong Luo <luost26@gmail.com>
2025-12-07 12:17:34 -0500

    update package metadata

--- prexsyn/__init__.py
+++ prexsyn/__init__.py
@@ -1,1 +1,10 @@
 __version__ = "0.1.0"
+
+try:
+    import prexsyn_engine  # noqa: F401
+except ImportError as e:
+    raise ImportError(
+        "PrexSyn Engine is not installed."
+        "Please refer to the documentation for installation instructions: "
+        "https://prexsyn.readthedocs.io/"
+    ) from e
